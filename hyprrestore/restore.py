47d4a58ec2cbf3bc188a9c2529ff28ab098a3f30
ysuovnii <ysuovnii@local>
2026-02-16 03:15:21 +0530

    create restore function

--- hyprrestore/restore.py
+++ hyprrestore/restore.py
@@ -1,20 +1,24 @@
 import json 
 import subprocess
 
-try : 
-    with open("./sessions/session.json", "r") as f :
-        clients = json.load(f)
+def restoreSession() :
+    try : 
+        with open("./sessions/session.json", "r") as f :
+            clients = json.load(f)
 
-        for client in clients :
-            app = clients[client]["class"]
+            for client in range(len(clients)) :
+                app = clients[client]["class"]
 
-            openApp = subprocess.Popen(
-                [app],
-                stdout=subprocess.DEVNULL,
-                stderr=subprocess.DEVNULL
-            )
-        print("Session Restored!")
-            
-except Exception as e : 
-    print("Exception : ", e)
-    exit(-1)
+                openApp = subprocess.Popen(
+                    [app],
+                    stdout=subprocess.DEVNULL,
+                    stderr=subprocess.DEVNULL
+                )
+            print("Session Restored!")
+                
+    except Exception as e : 
+        print("Exception : ", e)
+        exit(-1)
+
+if __name__ == "__main__" :
+    restoreSession()
